4073c8e1df5dcd1bd74fdd3f8963dcf140d09c83
syndagate <daniel4w@mail.de>
2024-01-03 13:56:05 +0100

    move treesitter config to plugin definition

--- files/lua/plugins/nvim-treesitter.lua
+++ files/lua/plugins/nvim-treesitter.lua
@@ -1,3 +1,35 @@
+local config = function()
+  require("nvim-treesitter.configs").setup({
+    indent = {
+      enable = true,
+    },
+    autotag = {
+      enable = true,
+    },
+    ensure_installed = {
+      "markdown",
+      "json",
+      "javascript",
+      "yaml",
+      "html",
+      "css",
+      "bash",
+      "lua",
+      "dockerfile",
+      "gitignore",
+      "python",
+      "php",
+    },
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = true,
+    },
+  })
+end
+
 return {
-  'nvim-treesitter/nvim-treesitter'
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  config = config,
 }
